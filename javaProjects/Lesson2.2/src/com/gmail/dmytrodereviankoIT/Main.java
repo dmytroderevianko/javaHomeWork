6c880edaf0e34794d6c61a73005b3f104bd37b53
Dmytro Derevianko <dmytrodereviankoIT>
2023-01-15 21:25:51 +0100

    Lesson2.2

---
+++ javaProjects/Lesson2.2/src/com/gmail/dmytrodereviankoIT/Main.java
@@ -0,0 +1,13 @@
+package com.gmail.dmytrodereviankoIT;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int appleCost = 2;
+		int quantity = 6;
+		int paid = appleCost * quantity;
+		System.out.println(paid + "USD");
+
+	}
+
+}
