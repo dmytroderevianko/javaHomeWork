643a4f9804304f9f6b06652598204f9c2689edc6
Dmytro Derevianko <dmytrodereviankoIT>
2023-01-25 12:39:14 +0100

    Lesson 3.3

---
+++ javaProjects/Lesson 3.3/src/com/gmail/dmytrodereviankoit/Main.java
@@ -0,0 +1,23 @@
+package com.gmail.dmytrodereviankoit;
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a;
+		System.out.println("Input side A");
+		a = sc.nextInt();
+		int b;
+		System.out.println("Input side B");
+		b = sc.nextInt();
+		int c;
+		System.out.println("Input side C");
+		c = sc.nextInt();
+		if (((a + c) > b) && ((b + c) > a) && ((a + b) > c)) {
+			System.out.println("This triangle exist");
+		} else {
+			System.out.println("This triangle don`t exist");
+		}
+	}
+
+}
